fe602198216a168033561b4717062a627fface31
Francesco De Carlo <decarlof@gmail.com>
2026-03-10 16:16:10 -0500

    fix: remove html_theme_path override that broke RTD search

--- docs/conf.py
+++ docs/conf.py
@@ -61,14 +61,8 @@ pygments_style = 'sphinx'
 # -- Options for HTML output ----------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_theme_options = {
     'style_nav_header_background': '#4f8fb8ff',
